327a7072d6a6c006e86434f208715472689b8c95
hector1905 <hector.sniper12@gmail.com>
2023-06-01 15:27:38 -0300

    Enviado o projeto para o repositório.

---
+++ sistema-gestao-estudantes/Login_Form.cs
@@ -0,0 +1,61 @@
+﻿using MySql.Data.MySqlClient;
+using sistema_gestao_estudantes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace sistema_gestao_estudantes
+{
+    public partial class Login_Form : Form
+    {
+        public Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Form_Load(object sender, EventArgs e)
+        {
+            // Define a imagem da picture box via código.
+            pictureBox1.Image = Image.FromFile("../../imagens/user.png");
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            MEU_BD bancoDeDados = new MEU_BD();
+            MySqlDataAdapter adaptador = new MySqlDataAdapter();
+            DataTable tabela = new DataTable();
+            MySqlCommand comando = new MySqlCommand("SELECT * FROM `usuario` WHERE `username` = @usn  AND `password` = @psd ", bancoDeDados.getConexao);
+            comando.Parameters.Add("@usn", MySqlDbType.VarChar).Value = txtUsuario.Text;
+            comando.Parameters.Add("@psd", MySqlDbType.VarChar).Value = txtSenha.Text;
+            adaptador.SelectCommand = comando;
+            adaptador.Fill(tabela);
+            if (tabela.Rows.Count > 0)
+            {
+                MessageBox.Show("SIM");
+            }
+            else
+            {
+                MessageBox.Show("Nome de usuario ou senha invalido", "erro de login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
---
+++ sistema-gestao-estudantes/MEU_BD.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace sistema_gestao_estudantes
+{
+    // A classe do nosso banco de dados.
+    internal class MEU_BD
+    {
+        // O "objeto" que representa a nossa conexão com o banco de dados.
+        private MySqlConnection conexao = new MySqlConnection("datasource=localhost;username=root;password=;database=t4_sga_bd");
+
+        
+
+
+
+
+        public MySqlConnection getConexao
+
+        {
+
+            get
+
+            {
+
+                return conexao;
+
+            }
+
+        }
+
+
+
+        public void abrirConexao()
+
+        {
+
+            if (conexao.State == ConnectionState.Closed)
+
+            {
+
+                conexao.Open();
+
+            }
+
+        }
+
+
+
+        public void fecharConexao()
+
+        {
+
+            if (conexao.State == ConnectionState.Open)
+
+            {
+
+                conexao.Close();
+
+            }
+
+        }
+    }
+}
